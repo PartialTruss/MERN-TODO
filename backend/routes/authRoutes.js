c55eba2bac30e9f2b700d9e9213df5941c53f1b6
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2025-04-12 15:58:37 +0330

    fix:bcrypt reinstalled

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import express from "express";
 import jwt from "jsonwebtoken"; // Import JWT library
 import User from "../models/userModel.js";
