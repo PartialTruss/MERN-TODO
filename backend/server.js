dce81205cf8eb18262da880d0d459bf7420e77ff
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2025-08-11 13:01:27 +0330

    fix:fix some issues about server.js

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,36 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+
 app.use(helmet());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://aquamarine-hummingbird-a4579f.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/quotes", quotesRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(
-    `Server running on https://aquamarine-hummingbird-a4579f.netlify.app/`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
