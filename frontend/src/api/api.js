de7c518572bd9d72f74f9626dd131737ce419d5a
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2025-08-11 13:05:40 +0330

    fix:base url fixed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/token_manager";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || "https://mern-todo-wvgj.onrender.com/api",
+  baseURL: "https://mern-todo-wvgj.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
