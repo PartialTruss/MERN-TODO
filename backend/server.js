9d73a66cc02506318d4982f05d5122a999bb772a
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2025-08-11 12:55:58 +0330

    new url added to server.js

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,7 @@ app.use("/api/users", userRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(
+    `Server running on https://aquamarine-hummingbird-a4579f.netlify.app/`
+  );
 });
